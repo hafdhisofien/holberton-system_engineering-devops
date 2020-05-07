4ef90e7842e41ce02845c98ac1433ea10a09e84d
hafdhisofien <1114@holbertonschool.com>
2020-05-07 20:40:25 +0000

    pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     r = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+                     .format(subreddit),
+                     headers={"User-Agent": "My-User-Agent"},
+                     allow_redirects=False)
     if r.status_code != 200:
         return 0
 
