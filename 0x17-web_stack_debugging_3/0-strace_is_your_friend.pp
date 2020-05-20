082d2a06fe63a200191a1b34b43ae31e6bd8ad8a
hafdhisofien <1114@holbertonschool.com>
2020-05-20 19:56:20 +0000

    oo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,2 @@
-#fixes a typo in the config 
-exec { 
-'/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': 
-}
+#fixes a typo in the config
+exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
